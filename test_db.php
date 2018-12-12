233851d6a19ac3f090441219653267a5581cdd1a
kanjanasid <42690814+kanjanasid@users.noreply.github.com>
2018-12-12 14:48:14 +0700

    Add files via upload

--- test_db.php
+++ test_db.php
@@ -7,7 +7,7 @@
  $conn = new mysqli($server, $username, $password, $db);
  mysqli_query($conn, "SET NAMES utf8");
  
- $sql_text = "SELECT * FROM inserttesting WHERE office LIKE = '%กฟจ%'";
+ $sql_text = "SELECT * FROM inserttesting WHERE office LIKE = '%กฟ%'";
  $query_text = mysqli_query ($conn,$sql_text);
  
  while($obj = mysqli_fetch_array($query_text))

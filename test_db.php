b575a31c7086e3c2d3c913bd02be46401b483a7f
kanjanasid <42690814+kanjanasid@users.noreply.github.com>
2018-12-13 14:42:36 +0700

    Add files via upload

--- test_db.php
+++ test_db.php
@@ -1,14 +1,12 @@
 <?php
-
-	 $server = "us-cdbr-iron-east-01.cleardb.net";
-	 $username = "b798786b8aa714";
-	 $password = "2e0e0451";
-	 $db = "heroku_ce52199dd4f50e1";
-	 $conn = new mysqli($server, $username, $password, $db);
-	 mysqli_query($conn, "SET NAMES utf8");
- 
 	function search($txtin)
 	{
+		$server = "us-cdbr-iron-east-01.cleardb.net";
+		$username = "b798786b8aa714";
+		$password = "2e0e0451";
+		$db = "heroku_ce52199dd4f50e1";
+		$conn = new mysqli($server, $username, $password, $db);
+		mysqli_query($conn, "SET NAMES utf8");
 		$sql_text = "SELECT * FROM inserttesting WHERE office LIKE '%".txtin."%'" ;
 		$query = mysqli_query($conn,$sql_text);
 		while ($obj_result = mysqli_fetch_array($query))

c2ee4d48b551c9eee4e93280f350d211b8048943
kanjanasid <42690814+kanjanasid@users.noreply.github.com>
2018-12-14 10:50:37 +0700

    Add files via upload

--- test-beer.php
+++ test-beer.php
@@ -1,4 +1,4 @@
-<html>// Tag เปิด
+<html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," charset="utf-8">
 		<title>PSQ V.2</title>
@@ -32,4 +32,4 @@
 			</div>
 		</div>
 	</body>
-</html>// Tag ปิด
+</html>
